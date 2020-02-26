45c9ae3cac776459a10e09918f5fafd3aeceb541
yusufzaini <60167358+yusufzaini@users.noreply.github.com>
2020-02-26 08:28:53 +0700

    Create exercise-5.js

---
+++ exercise-5.js
@@ -0,0 +1,63 @@
+//Soal 1
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+// console.log(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
+
+//Soal 2
+
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // do your own!
+var thirdWord = word[15] + word [16]; // do your own!
+var fourthWord = word[18] + word[19]; // do your own!
+var fifthWord = word[21] + word[22] + word[23] + word[24]; // do your own!
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+
+//Soal 3
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14); // do your own!
+var thirdWord3 = word.substring(15, 17); // do your own!
+var fourthWord3 = word.substring(18, 20); // do your own!
+var fifthWord3 = word.substring(21, 25); // do your own!
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+
+//Soal 54
+var word5 = 'wow JavaScript is so cool';
+var exampleFirstWord5 = word.substring(0, 3);
+var secondWord5 = word.substring(4, 14); // do your own!
+var thirdWord5 = word.substring(15, 17); // do your own!
+var fourthWord5 = word.substring(18, 20); // do your own!
+var fifthWord5 = word.substring(21, 25); // do your own!
+
+var firstWordLength = exampleFirstWord5.length;
+var secondWordLength = secondWord5.length;
+var thirdWordLength = thirdWord5.length;
+var fourthWordLength = fourthWord5.length;
+var fifthWordLength = fifthWord5.length;
+
+
+
+console.log('First Word: ' + exampleFirstWord5 + ' with length : ' + firstWordLength);
+console.log('Second Word: ' + secondWord5 + ' with length : ' + secondWordLength);
+console.log('Third Word: ' + thirdWord5 + ' with length : ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord5 + ' with length : ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord5 + ' with length : ' + fifthWordLength);
