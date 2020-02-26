e3cf6992534035bb28ae96afb556b74e068a6bae
yusufzaini <60167358+yusufzaini@users.noreply.github.com>
2020-02-26 08:28:50 +0700

    Create exercise-4.js

---
+++ exercise-4.js
@@ -0,0 +1,44 @@
+var tanggal= 08;
+var bulan= 06;
+var tahun= 2019;
+
+switch(bulan){
+    case 1:
+        bulan = "Januari";
+    break;
+    case 2:
+        bulan = "Februari"
+    break;
+    case 3:
+        bulan = "Maret"
+    break;
+    case 4:
+        bulan = "April"
+    break;
+    case 5:
+        bulan = "Mei"
+    break;
+    case 6:
+        bulan = "Juni"
+    break;
+    case 7:
+        bulan = "Juli"
+    break;
+    case 8:
+        bulan = "Agustus"
+    break;
+    case 9:
+        bulan = "September"
+    break;
+    case 10:
+        bulan = "Oktober"
+    break;
+    case 11:
+        bulan = "November"
+    break;
+    case 12:
+        bulan = "Desember"
+    break;
+}
+
+console.log(tanggal + " " + bulan + " " + tahun);
